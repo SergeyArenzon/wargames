5999a4a94dbb574dc92c34711fecae85b219b6e1
SergeyArenzon <sergeyr1991@gmail.com>
2020-05-21 17:35:38 +0300

    first commit

---
+++ Soldier.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdlib.h>
+#include <iostream>
+
+class Soldier{
+    public:
+    int  health_points;
+    int damage;
+    int num_player;
+
+    Soldier(){}
+
+    int getNum();
+    int getHealth();
+    int getDamge();
+    void attack(Soldier s);
+    
+
+
+
+};
---
+++ Test.cpp
@@ -0,0 +1,22 @@
+#include "doctest.h"
+#include <iostream>
+#include <stdexcept>
+#include "DemoGame.hpp"
+#include "FootSoldier.hpp"
+#include "FootCommander.hpp"
+
+
+
+
+using namespace std;
+
+
+#include "DemoGame.hpp"
+
+
+
+TEST_CASE("Test case 1") {
+   	for(int i = 0 ; i < 100 ; i++){
+        CHECK(true);
+    }
+}
