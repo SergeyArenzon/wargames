75860d16c0af124d649585015367b630beb0e4a3
SergeyArenzon <sergeyr1991@gmail.com>
2020-05-25 12:28:44 +0300

    test3

--- Board.cpp
+++ Board.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 namespace WarGame {
+      bool Board::has_soldiers(uint player_number) const{return true;}
 
-
+    Soldier*& Board::operator[](std::pair<int, int>){}
+    
+    void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction){}
          
 }
